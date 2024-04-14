016f52e171e02ca8a1110c45a5899f444bbf80e8
TheLongIslander <157660154+TheLongIslander@users.noreply.github.com>
2024-04-13 20:51:01 -0400

    Add files via upload

---
+++ public/login.js
@@ -0,0 +1,32 @@
+document.getElementById('login-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    fetch('/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password })
+    })
+    .then(response => response.json())
+    .then(data => 
+        {
+        if (data.token) 
+        {
+            // Store the token in localStorage
+            localStorage.setItem('token', data.token);
+            // Redirect to the control panel page
+            window.location.href = '/index.html';
+        } 
+        else 
+        {
+            alert('Login failed: Incorrect Password');
+        }
+    })
+    .catch(err => {
+        alert('Login failed');
+    });
+});
